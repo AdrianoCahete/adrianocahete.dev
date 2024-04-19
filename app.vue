20125b1fd5300f691413e0b97dce5a874c40106c
Adriano Cahete <me@adrianocahete.dev>
2024-04-19 11:36:12 -0300

    feat(app): simple first version

--- app.vue
+++ app.vue
@@ -1,50 +1,79 @@
 <script setup>
-const links = [{
-  label: 'Features',
-  to: '#features',
-  exactHash: true
-}, {
-  label: 'Documentation',
-  to: 'https://ui.nuxt.com/pro',
-  target: '_blank'
-}]
+const links = [
+  {
+    label: "About",
+    to: "#about",
+    exactHash: true,
+  },
+  {
+    label: "Work",
+    to: "#work",
+    exactHash: true,
+  },
+];
 
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
-const title = 'Nuxt UI Pro - Starter'
-const description = 'Nuxt UI Pro is a collection of premium Vue components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.'
+const title = "Adriano Cahete";
+const description =
+  "Nuxt UI Pro is a collection of premium Vue components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.";
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui-pro-starter.nuxt.dev/social-card.png',
-  twitterImage: 'https://ui-pro-starter.nuxt.dev/social-card.png',
-  twitterCard: 'summary_large_image'
-})
+  ogImage: "https://ui-pro-starter.nuxt.dev/social-card.png",
+  twitterImage: "https://ui-pro-starter.nuxt.dev/social-card.png",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
   <UHeader :links="links">
-    <template #logo>
-      Nuxt UI Pro <UBadge label="Starter" variant="subtle" class="mb-0.5" />
-    </template>
+    <template #logo> Adriano Cahete </template>
 
     <template #right>
       <UColorModeButton />
 
-      <UButton to="https://github.com/nuxt-ui-pro/starter" target="_blank" icon="i-simple-icons-github" aria-label="GitHub" color="gray" variant="ghost" />
+      <UButton
+        to="https://linkedin.com/in/AdrianoCahete"
+        target="_blank"
+        icon="i-simple-icons-linkedin"
+        aria-label="LinkedIn"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        to="https://github.com/AdrianoCahete"
+        target="_blank"
+        icon="i-simple-icons-github"
+        aria-label="GitHub"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        to="https://www.behance.net/AdrianoCahete/"
+        target="_blank"
+        icon="i-simple-icons-behance"
+        aria-label="Behance"
+        color="gray"
+        variant="ghost"
+      />
+      <UButton
+        to="https://adrianocahete.dev/resume-artifacts/[en-US]-Adriano_Cahete.pdf"
+        target="_blank"
+        icon="mdi:file-pdf-box"
+        aria-label="PDF Resume"
+        color="gray"
+        variant="ghost"
+      />
     </template>
   </UHeader>
 
@@ -53,8 +82,6 @@ useSeoMeta({
   </UMain>
 
   <UFooter>
-    <template #left>
-      Copyright © {{ new Date().getFullYear() }}
-    </template>
+    <template #left> Copyright © {{ new Date().getFullYear() }} </template>
   </UFooter>
 </template>
---
+++ components/page-about.vue
@@ -0,0 +1,23 @@
+<template>
+  <UPageGrid>
+    <UPageCard
+      icon="i-heroicons-wrench-screwdriver"
+      title="Fully customizable"
+      description="Like Nuxt UI, change the style of any component from your App Config or customize them specifically through the ui prop."
+    />
+    <UPageCard
+      icon="i-heroicons-square-3-stack-3d"
+      title="Slots for everything"
+      description="Each component leverages the power of Vue's slots to give you the flexibility to build anything."
+    />
+    <UPageCard
+      icon="i-heroicons-device-phone-mobile"
+      title="Responsive by design"
+      description="Nuxt UI Pro components aims to structure your content, they are responsive by design and will adapt to any screen size."
+    />
+  </UPageGrid>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
