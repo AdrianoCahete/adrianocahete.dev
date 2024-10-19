84ea63555a2f303a4db47499dbf8efc8bd46b656
Adriano Cahete <me@adrianocahete.dev>
2024-10-18 21:30:17 -0300

    feat(app): fix scss compiler legacy notification + add proper build date to version footer

--- components/nav-footer.vue
+++ components/nav-footer.vue
@@ -1,9 +1,16 @@
 <template>
   <UFooter>
-    <template #left> Copyright © {{ new Date().getFullYear() }} </template>
+    <template #left>
+      Last update: {{ new Date().getFullYear() }}.{{
+        new Date().getMonth() + 1
+      }}
+      - v{{ pkg.public.version }}
+    </template>
   </UFooter>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const pkg = useRuntimeConfig();
+</script>
 
 <style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import pkg from "./package.json";
+
 export default defineNuxtConfig({
   extends: ["@nuxt/ui-pro"],
   modules: ["@vueuse/nuxt", "@nuxt/ui", "@nuxtjs/google-fonts"],
 
+  runtimeConfig: {
+    // Private keys are only available on the server
+    // apiSecret: '',
+
+    // Public keys that are exposed to the client
+    public: {
+      pkgVersion: pkg.version,
+    },
+  },
+
   ui: {
     icons: ["heroicons", "simple-icons"],
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
   },
 
   devtools: {
