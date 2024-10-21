03e33ed4dc34b6510e56072dfc5c8d3f8d758ab7
Adriano Cahete <me@adrianocahete.dev>
2024-10-20 21:49:35 -0300

    feat(app): add more projects + change card component on project page

--- components/page/projects.vue
+++ components/page/projects.vue
@@ -2,7 +2,7 @@
   <section>
     <h2 class="text-subsection">Projects</h2>
     <UPageGrid>
-      <UPageCard
+      <ULandingCard
         v-for="(project, index) in projects"
         :key="index"
         :title="project.title"
@@ -26,19 +26,33 @@
 </template>
 
 <script lang="ts" setup>
-const projects = [
-  {
-    title: "phpMyAdmin Website",
-    description: "Redesign and brand update to phpMyAdmin project",
-    icon: "",
-    url: "https://github.com/phpmyadmin/website/pull/137",
-  },
-];
+  const projects = [
+    {
+      title: "phpMyAdmin Website",
+      description: "Redesign and brand update to phpMyAdmin project",
+      icon: "",
+      url: "https://github.com/phpmyadmin/website/pull/137",
+    },
+    {
+      title: "Google Chrome Lighthouse",
+      description:
+        "Automated auditing, performance metrics, and best practices for the web",
+      icon: "",
+      url: "https://github.com/GoogleChrome/lighthouse/blob/468c10bb9df7ded215b7a3c47e23f48c1af656f7/changelog.md#new-contributors-24",
+    },
+    {
+      title: "RocketChat",
+      description:
+        "Work as a Product Designer and update stylesheet architecture to meet best practices",
+      icon: "",
+      url: "https://github.com/GoogleChrome/lighthouse/blob/468c10bb9df7ded215b7a3c47e23f48c1af656f7/changelog.md#new-contributors-24",
+    },
+  ];
 </script>
 
 <style lang="scss">
-.text-subsection {
-  margin-bottom: 0.4rem;
-  font-size: 1.4rem;
-}
+  .text-subsection {
+    margin-bottom: 0.4rem;
+    font-size: 1.4rem;
+  }
 </style>
