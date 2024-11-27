0017120e0f29a151caaf2d7bf170a50f7b488500
Adriano Cahete <me@adrianocahete.dev>
2024-11-27 00:00:44 -0300

    feat(app): add timeline component

---
+++ app/components/nav/timeline/timeline.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="['nav-timeline', isCenter ? ' is-center' : '']">
+    <slot name="timelineitem"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineProps({
+    isCenter: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .nav-timeline {
+    display: flex;
+    flex-direction: column;
+    width: max-content;
+    padding-top: 50px;
+    padding-bottom: 50px;
+
+    &.is-center {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+</style>
