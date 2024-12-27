7df72636e378c05c49a6497af99449142278d8a6
Adriano Cahete <me@adrianocahete.dev>
2024-12-27 17:06:15 -0300

    feat(app): remove nuxt 4 compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ import pkg from "./package.json";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/docs/getting-started/upgrade#opting-in-to-nuxt-4
-  future: {
-    compatibilityVersion: 4,
-  },
+  // future: {
+  //   compatibilityVersion: 4,
+  // },
 
   extends: ["@nuxt/ui-pro"],
   modules: [
