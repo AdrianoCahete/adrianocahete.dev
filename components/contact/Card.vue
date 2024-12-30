783495d88e358fde1e8dac61aa1bbe17cc244bf5
Adriano Cahete <me@adrianocahete.dev>
2024-12-30 18:41:00 -0300

    feat(app): add vueuse + create contact item component

---
+++ components/contact/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="['contact-list', $device.isMobile ? 'contact-mobile' : '']">
+    <slot name="contactItem"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineProps({
+    isCenter: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .contact {
+    display: flex;
+    flex-direction: column;
+    max-width: max-content; // TODO: Fix this on mobile
+    width: 80vw; // TODO: Fix this on mobile
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+
+    &.contact-mobile {
+      padding-top: 0;
+    }
+  }
+</style>
---
+++ components/contact/item.vue
@@ -0,0 +1,121 @@
+<template>
+  <div
+    :class="['contact_item', $device.isMobile ? ' contact-mobile' : '']"
+  >
+    <UButton
+        class="p-2"
+        :to="getUrlPerService(service)"
+        :target="target"
+        :icon="getIconPerService(service)"
+        :aria-label="service"
+        color="black"
+        variant="ghost"
+        :rel="itsMe"
+      />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineProps({
+    service: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    target: {
+      type: String,
+      required: false,
+      default: "_blank",
+      validator: (target: string) => {
+        return ['_blank', '_self'].includes(target);
+      },
+    },
+    iconColor: {
+      type: String,
+      required: false,
+    },
+    variant: {
+      type: String,
+      required: false,
+      default: "ghost",
+    },
+    itsMe: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  });
+
+  function getIconPerService(service: string) {
+    switch (service) {
+      case "linkedin":
+        return "i-simple-icons-linkedin";
+
+      case "github":
+        return "i-simple-icons-github";
+
+      case "email":
+        return "i-simple-icons-gmail";
+
+      case "behance":
+        return "i-simple-icons-behance";
+
+      case "resume":
+        return "i-simple-icons-whatsapp";
+
+      default:
+        return "no-icon";
+    }
+  }
+
+  function getUrlPerService(service: string) {
+    switch (service) {
+      case "linkedin":
+        return "https://www.linkedin.com/in/AdrianoCahete/";
+
+      case "github":
+        return "https://github.com/AdrianoCahete";
+
+      case "email":
+        return "me@adrianocahete.dev";
+
+      case "behance":
+        return "https://www.behance.net/AdrianoCahete/";
+
+      case "resume":
+        return "https://adrianocahete.dev/assets/Adriano_Cahete.pdf";
+
+      default:
+        return "no-url";
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .contact_item {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+
+    &:hover {
+      border-image: linear-gradient(
+        0deg,
+        var(--primaryColor) 0%,
+        rgb(var(--color-gray-900) / var(--tw-bg-opacity, 1)) 50%,
+        var(--primaryColor) 100%
+      );
+      border-image-slice: 0 0 0 1;
+      border-image-width: 1px; // TODO: Get from vars
+      border-left-color: transparent;
+      transition: border-left-color 0.5s ease-in-out;
+
+      & .contact_item-dot {
+        background-color: var(--primaryColor);
+        transition: background-color 0.7s ease-in-out;
+      }
+    }
+
+    // &.contact-mobile {
+    // }
+  }
+</style>
